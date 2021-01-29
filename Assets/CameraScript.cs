ad8871ef280a35379234ec77823ddb14483ae64a
Tyler Weeres <tweeres04@gmail.com>
2021-01-29 08:03:09 -0800

    Camera controls

---
+++ Assets/CameraScript.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+    public float panSpeed = 30f;
+    public float panBorderThickness = 10f;
+
+    public float scrollSpeed = 5f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("w") || Input.mousePosition.y >= Screen.height - panBorderThickness)
+        {
+            transform.Translate(Vector3.forward * panSpeed * Time.deltaTime, Space.World);
+        }
+        if (Input.GetKey("s") || Input.mousePosition.y <= panBorderThickness)
+        {
+            transform.Translate(Vector3.back * panSpeed * Time.deltaTime, Space.World);
+        }
+        if (Input.GetKey("d") || Input.mousePosition.x >= Screen.width - panBorderThickness)
+        {
+            transform.Translate(Vector3.right * panSpeed * Time.deltaTime, Space.World);
+        }
+        if (Input.GetKey("a") || Input.mousePosition.x <= panBorderThickness)
+        {
+            transform.Translate(Vector3.left * panSpeed * Time.deltaTime, Space.World);
+        }
+
+        float scroll = Input.GetAxis("Mouse ScrollWheel");
+
+        Vector3 pos = transform.position;
+
+        pos.y -= scroll * 1000 * scrollSpeed * Time.deltaTime;
+
+        transform.position = pos;
+    }
+}
---
+++ To Do.md
@@ -0,0 +1,9 @@
+# Possible next steps
+
+- Add more control to the workers
+- Add more resources farther away
+- Build more workers
+- Worker upgrades
+  - Increased speed
+  - Carry more at a time
+- View controls ✅
