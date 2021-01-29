66ce0b845a4994f97d95c39aef732a2f947a5ea8
Tyler Weeres <tweeres04@gmail.com>
2021-01-28 22:53:34 -0800

    Added a straightforward UI to show the total minerals collected

---
+++ Assets/MineralsCollectedUI.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class MineralsCollectedUI : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetComponent<Text>().text = Base.instance.minerals.ToString();
+    }
+}
